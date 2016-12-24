16a481363c2873dbc6c55c73c7c664b9758ed68e
Minhas Kamal <minhaskamal024@gmail.com>
2016-12-24 21:08:16 +0600

    Consumes full space of the RAM.

--- ram_slayer.c
+++ ram_slayer.c
@@ -1,5 +1,5 @@
 // Source: https://www.quora.com/What-is-your-smallest-piece-of-code-that-can-crash-the-computer/answer/George-Gonzalez-5?srid=u2HNX
-// Usees up all space of RAM.
+// Consumes full space of the RAM.
 
 main(){
     while(malloc(1000));
